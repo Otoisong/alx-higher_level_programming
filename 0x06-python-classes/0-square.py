f51a4322658c7ec2f7e81a3e42d27f2f2833090a
Otoisong <tolysong4best@gmail.com>
2022-12-22 08:34:07 -0800

    Python class & Object

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""
-   This is an Empty Class Square
-   """
+"""This is an Empty Class Square"""
 
 
 class Square:
-    """
-    class: This Class (Square) is to define a square but it's empty.
-    """
+    """Define Class Square"""
+    pass
